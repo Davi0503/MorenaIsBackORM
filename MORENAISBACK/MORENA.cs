84e9640977ae76ebeee197984a7f3658a6325294
Davi0503 <davitavares0503@gmail.com>
2019-07-18 23:07:08 -0300

    inicio de um imperio

---
+++ MORENAISBACK/MORENA.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace MORENAISBACK
+{
+    public class MORENA
+    {
+        private string connectionString = "Data Source= DESKTOP-JR0T6JP; Initial Catalog= MORENAISBACK; integrated security=true";
+
+        public List<string> Select(string nomeTabela)
+        {
+
+
+            string query = "SELECT NOME FROM " + nomeTabela + ";";
+            List<string> lista = new List<string>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    //cmd.Parameters.AddWithValue("@NOMETABELA", nomeTabela);
+
+                    SqlDataReader sdr = cmd.ExecuteReader();
+
+                    while (sdr.Read())
+                    {
+                        string pao = sdr["NOME"].ToString();
+                        lista.Add(pao);
+                    }    
+                }
+            } 
+
+                return lista;
+        }
+        
+
+    }
+}
---
+++ TESTEMORENA/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MORENAISBACK;
+
+namespace TESTEMORENA
+{
+    public class Program
+    {
+        static MORENA _morena = new MORENA();
+
+        public Program()
+        {
+            _morena = new MORENA();
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            string nomeTabela = "batatinha";
+            foreach (string item in _morena.Select(nomeTabela))
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
